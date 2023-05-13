025b27371f48bc44137e69004a46856bd5b04d0d
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-13 19:24:29 +0200

    Update databaza.sql

--- SQL/databaza.sql
+++ SQL/databaza.sql
@@ -1,46 +1,9 @@
-create database KNK;
-use KNK;
+create database knk;
+use knk;
 
-
-CREATE TABLE Student
-(
-    studentID VARCHAR(255) not null,
-    studentName VARCHAR(255) not null,
-    studentSurname VARCHAR(255) not null,
-    gender CHAR(1) not null,
-    phoneNumber VARCHAR(255) not null,
-    email VARCHAR(255) not null unique,
-    password VARCHAR(255) not null,
-    PRIMARY KEY(studentID)
+create table login(
+username nvarchar(30)not null,
+email nvarchar(30) not null,
+id nvarchar(30) primary key not null,
+password nvarchar(50)not null
 );
-
-CREATE TABLE Administrator
-(
-    administratorID VARCHAR(20),
-    name VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    PRIMARY KEY(administratorID)
-    );
-
-
-    create table Exames(
-    exameID VARCHAR(20) not null,
-    class VARCHAR(255),
-    price double,
-    times int,
-    PRIMARY KEY(exameID)
-    );
-
-    create table semester(
-     semNum VARCHAR(20) not null,
-    classes VARCHAR(255),
-    price double,
-    year int
-    );
-
-
-INSERT INTO Administrator VALUES
-    ('1','admin','admin','admin1');
-
-
