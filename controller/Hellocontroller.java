eb35b2fe853af2e79fd8e0c93b1e4e48177b6770
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-13 20:03:33 +0200

    Update Hellocontroller.java

--- controller/Hellocontroller.java
+++ controller/Hellocontroller.java
@@ -1,12 +1,9 @@
-package com.example.projectknk;
-
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 
 public class HelloController {
@@ -37,7 +34,7 @@ public class HelloController {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/knk";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_PASSWORD = "Lana.1234";
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
@@ -45,12 +42,25 @@ public class HelloController {
 
     public void handleLoginButtonAction() {
         try (Connection connection = getConnection()) {
-
             String query = "SELECT * FROM users WHERE username = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username.getText());
+            ResultSet resultSet = statement.executeQuery();
 
+            if (resultSet.next()) {              
+                String storedPassword = resultSet.getString("password");
+                String enteredPassword = password.getText();
+
+                if (storedPassword.equals(enteredPassword)) {                   
+                    log.setText("Login successful");                  
+                } else {
+                    log.setText("Invalid password");
+                }
+            } else {
+                log.setText("User not found");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
-
         }
     }
 };
