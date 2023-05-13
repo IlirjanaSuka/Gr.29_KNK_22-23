98818c42e66ce112008bb20cadfd06081aa0c6eb
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-13 20:04:58 +0200

    Update database.java

--- database/database.java
+++ database/database.java
@@ -3,12 +3,6 @@ package com.example.projectknk;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author WINDOWS 10
- * SUBSCRIBE OUR YOUTUBE CHANNEL -> https://www.youtube.com/channel/UCPgcmw0LXToDn49akUEJBkQ
- * THANKS FOR YOUR SUPPORT : )
- */
 public class database {
 
     public static Connection connectDb(){
