6a7f50cf8925df5167139e89aca5a21c27c43e1c
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-18 03:16:20 +0200

    Update databaza.sql

--- SQL/databaza.sql
+++ SQL/databaza.sql
@@ -1,9 +1,10 @@
 create database knk;
 use knk;
 
-create table login(
-username nvarchar(30)not null,
-email nvarchar(30) not null,
-id nvarchar(30) primary key not null,
-password nvarchar(50)not null
+create table admin(
+id int primary key,
+username nvarchar(100),
+password nvarchar(100)
 );
+
+insert into admin(id, username, password) value(1, 'admin1', '123');
