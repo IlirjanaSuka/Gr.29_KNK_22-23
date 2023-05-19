3a5ea5f2e95cb5be63d5eed06494146ebefaf946
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-19 19:15:35 +0200

    Update HelloApplication.java

--- application/HelloApplication.java
+++ application/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.loginstyle;
+package com.example.demo16;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Login");
+        stage.setTitle("Login!");
         stage.setScene(scene);
         stage.show();
     }
