44defbf9088180a00371be4dcfabd21721bc2c37
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-19 19:18:37 +0200

    Create Dashboard.java

---
+++ application/Dashboard.java
@@ -0,0 +1,22 @@
+package com.example.demo16;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Dashboard extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Dashboard.class.getResource("dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
